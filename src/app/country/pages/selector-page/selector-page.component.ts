7d76008131a9d137aa55aa52aaf6a499b29d8b70
Daniela Liendo <daniela@ualabee.com>
2023-05-30 22:22:01 +0200

    selector page component was created

---
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styleUrls: ['./selector-page.component.css']
+})
+
+export class SelectorPageComponent {
+
+}
