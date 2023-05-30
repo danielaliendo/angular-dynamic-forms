fd7bbbe9257037e0a4d4ab521345a51ff9f90183
Daniela Liendo <daniela@ualabee.com>
2023-05-30 22:28:37 +0200

    component selector name was changed to country-selector-page

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-selector-page',
+  selector: 'country-selector-page',
   templateUrl: './selector-page.component.html',
   styleUrls: ['./selector-page.component.css']
 })
