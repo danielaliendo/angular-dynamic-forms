beadc988a5b6fd32c541f15d7927c077df0ce33b
Daniela Liendo <daniela@ualabee.com>
2023-05-30 22:26:54 +0200

    selector page component was imported

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectorPageComponent } from "./pages/selector-page/selector-page.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SelectorPageComponent
+  ],
   imports: [
     CommonModule
   ]
