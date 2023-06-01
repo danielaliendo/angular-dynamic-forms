3713821c69c9049bd027a6a4e3e92054cc1bf9c0
Daniela Liendo <daniela@ualabee.com>
2023-06-01 21:41:35 +0200

    getCountriesByRegion, getCountryByAlphaCode and getCountriesByCode methods were added

--- src/app/country/services/countries.service.ts
+++ src/app/country/services/countries.service.ts
@@ -1,22 +1,60 @@
-import { Injectable } from '@angular/core';
-import { Region, SmallCountry } from "../interfaces/country.interfaces";
+import {Injectable} from '@angular/core';
+import {Country, Region, SmallCountry} from "../interfaces/country.interfaces";
+import {combineLatest, map, Observable, of, tap} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class CountriesService {
+  private baseUrl = 'https://restcountries.com/v3.1'
 
-  private  _regions: Region[] = [Region.Africa, Region.Asia, Region.Americas, Region.Europe, Region.Oceania]
+  private _regions: Region[] = [Region.Africa, Region.Asia, Region.Americas, Region.Europe, Region.Oceania]
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {
+  }
 
   get regions(): Region[] {
     return [...this._regions]
   }
 
-  public getCountriesByRegion(region: Region) : SmallCountry[] {
-    return []
+  public getCountriesByRegion(region: Region, fields = 'cca3,name,borders'): Observable<SmallCountry[]> {
+    if (!region) return of([]);
+    return this.http.get<Country[]>(`${this.baseUrl}/region/${region}?fields=${fields}`)
+      .pipe(
+        map(countries => countries.map(country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? []
+        })))
+      )
+  }
+
+  public getCountryByAlphaCode(alphaCode: string, fields = 'cca3,name,borders'): Observable<SmallCountry> {
+    return this.http.get<Country>(`${this.baseUrl}/alpha/${alphaCode}?fields=${fields}`)
+      .pipe(
+        map(country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? []
+        }))
+      )
+  }
+
+  public getCountriesByCode(borders: string[]): Observable<SmallCountry[]> {
+    if (!borders || borders.length === 0) return of([])
+
+    const countriesRequests: Observable<SmallCountry>[] = []
+
+    borders.forEach(code => {
+      const request: Observable<any> = this.getCountryByAlphaCode(code);
+      countriesRequests.push(request)
+    })
+
+    return combineLatest(countriesRequests)
   }
 
 }
