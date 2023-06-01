7b9a3b30997c510cd16c5d36b5a18e23c52c6e2e
Daniela Liendo <daniela@ualabee.com>
2023-06-01 09:06:56 +0200

    get regions and onRegionChange methods were added

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CountriesService} from "../../services/countries.service";
+import {Region} from "../../interfaces/country.interfaces";
 
 @Component({
   selector: 'country-selector-page',
@@ -7,7 +9,7 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./selector-page.component.css']
 })
 
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
 
   public form: FormGroup = this.fb.group({
     region: ['', Validators.required],
@@ -15,6 +17,25 @@ export class SelectorPageComponent {
     borders: ['', Validators.required]
   })
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.onRegionChange();
+  }
+
+  get regions(): Region[] {
+    return this.countriesService.regions
+  }
+
+  onRegionChange(): void {
+    this.form.get('region')?.valueChanges
+      .subscribe((region: Region) => {
+        console.log(region)
+      })
+  }
 
 }
