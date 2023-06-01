cb17c72d8eb848f12714c4abe457ecba0c98a79b
Daniela Liendo <daniela@ualabee.com>
2023-06-01 09:05:57 +0200

    get regions and getCountriesByRegion methods were created

--- src/app/country/services/countries.service.ts
+++ src/app/country/services/countries.service.ts
@@ -10,4 +10,13 @@ export class CountriesService {
   private  _regions: Region[] = [Region.Africa, Region.Asia, Region.Americas, Region.Europe, Region.Oceania]
 
   constructor() { }
+
+  get regions(): Region[] {
+    return [...this._regions]
+  }
+
+  public getCountriesByRegion(region: Region) : SmallCountry[] {
+    return []
+  }
+
 }
